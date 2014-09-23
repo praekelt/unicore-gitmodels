d6c55d2eb3bb3359a5bacc7c049985d1faaab5b6
Milton Madanda <madandat@gmail.com>
2014-09-23 14:33:51 +0200

    added source tests

--- unicore_gitmodels/tests/test_models.py
+++ unicore_gitmodels/tests/test_models.py
@@ -49,27 +49,35 @@ class ModelsTestCase(unittest.TestCase):
     def test_category_language_filter(self):
         models = self.get_repo_models()
 
-        models.GitCategoryModel(
+        cat = models.GitCategoryModel(
             title='Diarrhoea', slug='diarrhoea', language='eng-UK'
-        ).save(True, message='added diarrhoea Category')
-        models.GitCategoryModel(
-            title='Diarrhoea', slug='diarrhoea', language='eng-US'
-        ).save(True, message='added diarrhoea Category')
+        )
+        cat.save(True, message='added diarrhoea Category')
+
+        cat2 = models.GitCategoryModel(
+            title='Diarrhoea', slug='diarrhoea', language='eng-US', source=cat
+        )
+        cat.save(True, message='added diarrhoea Category')
 
         self.assertEquals(
             len(models.GitCategoryModel.filter(language='eng-UK')), 1)
+        self.assertEquals(cat2.source.uuid, cat.uuid)
 
     def test_page_language_filter(self):
         models = self.get_repo_models()
 
-        models.GitPageModel(
+        p = models.GitPageModel(
             title='Test Page 1', content='this is sample content for pg 1',
             language='eng-UK'
-        ).save(True, message='added diarrhoea Category')
-        models.GitPageModel(
+        )
+        p.save(True, message='added diarrhoea Category')
+
+        p2 = models.GitPageModel(
             title='Test Page 1', content='this is sample content for pg 1',
-            language='eng-US'
-        ).save(True, message='added diarrhoea Category')
+            language='eng-US', source=p
+        )
+        p2.save(True, message='added diarrhoea Category')
 
         self.assertEquals(
             len(models.GitPageModel.filter(language='eng-UK')), 1)
+        self.assertEquals(p2.source.uuid, p.uuid)
