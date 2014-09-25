da674db4aebc1ee237bafce0c41b2439028455af
Simon de Haan <simon@praekeltfoundation.org>
2014-09-25 15:41:23 +0200

    add featured_in_category boolean field

--- unicore_gitmodels/models.py
+++ unicore_gitmodels/models.py
@@ -92,6 +92,7 @@ class GitPageModel(SlugifyMixin, FilterMixin, models.GitModel):
     modified_at = fields.DateTimeField(required=False)
     published = fields.BooleanField(default=True)
     primary_category = fields.RelatedField(GitCategoryModel, required=False)
+    featured_in_category = fields.BooleanField(default=False)
     language = fields.CharField(required=False)
     source = fields.RelatedField('GitPageModel', required=False)
 
@@ -119,4 +120,5 @@ class GitPageModel(SlugifyMixin, FilterMixin, models.GitModel):
             'modified_at': self.modified_at,
             'published': self.published,
             'primary_category': primary_category,
+            'featured_in_category': self.featured_in_category,
         }
